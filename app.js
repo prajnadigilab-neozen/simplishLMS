50535d16c8006f963b03801150fc739c0612715d
Prajna DigiLab <prajna.digilab@gmail.com>
2026-06-12 21:37:48 +0530

    Instrument app.js to intercept process.exit and log call stack

--- app.js
+++ app.js
@@ -20,6 +20,14 @@ process.on('uncaughtException', (err) => {
     process.exit(1);
 });
 
+// Override process.exit to capture silent exit calls
+const originalExit = process.exit;
+process.exit = function(code) {
+    const err = new Error(`process.exit(${code}) was called!`);
+    logEmergency(err);
+    originalExit.apply(this, arguments);
+};
+
 process.on('unhandledRejection', (reason) => {
     logEmergency(reason instanceof Error ? reason : new Error(String(reason)));
     process.exit(1);
